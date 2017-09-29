5225c840b307f3ce36ee630abf636a12af181507
guchengfeng <lujianfei@lujianfei-Mac-mini.local>
2017-09-29 16:40:19 +0800

    Initial Commit

---
+++ GCFVOIP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCFVOIP
+//
+//  Created by lujianfei on 2017/9/29.
+//  Copyright © 2017年 com.guchengfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCFVOIP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCFVOIP
+//
+//  Created by lujianfei on 2017/9/29.
+//  Copyright © 2017年 com.guchengfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
